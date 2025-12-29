c942239a2f8225bb9919ac3fbaf08dd7e7318efd
Wuttisak0907 <Wuttisak.ka@ku.th>
2025-12-29 17:01:05 +0700

    js file

---
+++ Js/main.js
@@ -0,0 +1,40 @@
+/*
+&& และ
+||หรือ
+! not หรือ ไม่ 
+*/
+/*
+let number1 =5  
+let number2 =8
+// 
+let condition = !(number1 >= 3 || number2 >=10);
+console.log("condition :", condition);
+
+let age =30
+let gender=" ชาย"
+
+if (age >= 20 && gender == "ชาย "){
+    console.log(" ผู้ชายที่มีอายุ 20 ปีขึ้นไป ");
+}
+*/
+/*
+let number = 20 
+if (number %2 !=0){
+    console.log("เป็นเลขคู่")
+}
+*/
+
+/*
+loop 
+while
+for
+*/
+let counter = 0;
+while(counter < 10){
+    console.log('Hello World');
+    //counter = counter+1;
+    counter+= 1;
+}
+for(let i=0; i<10;i++){
+    console.log('Hello World from for loop');
+}

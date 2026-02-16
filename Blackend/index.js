a8971a2e3fc574b73e94eb28b72c6042b9d3e2e9
Wuttisak0907 <wuttisak.ka@ku.th>
2026-02-16 16:47:27 +0700

    Update index.js

--- Blackend/index.js
+++ Blackend/index.js
@@ -1,17 +1,63 @@
-//ทำการimport http module
-const http = require('http');
-const host = 'localhost';
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 const port = 8000;
 
-//กำหนดค่าเริ่มต้นของserver เมื่อเปิดใช้งาน เว็บไซต์
+app.use(bodyParser.json());
 
-const requireListener = function (req, res) {
-    res.writeHead(200);
-    res.end('My First Server ');
-}
+let users = [];
+let counter = 1;
 
-//run
-const server = http.createServer(requireListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+// paht = get/users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path = Post/user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter++;
+    users.push(user);
+    res.json({ message: 'User added successfully', user: user });x
+});
+
+//path = put/user/:id
+app.patch('/user/:id', (req, res) => {
+   let id = req.params.id;
+   let updatedUser = req.body;
+   //หา user จาก id
+    let selectedindex = users.findIndex(user => user.id == id);
+   //update user
+   if (updatedUser.name ) {
+    users[selectedindex].name = updatedUser.name;
+    }
+    if (updatedUser.age) {
+        users[selectedindex].age = updatedUser.age;
+    }
+   //ส่ง response กลับไป
+    res.json({ 
+    message: 'User updated successfully',
+    data : {
+        user : updatedUser,
+        indexUpdate : selectedindex
+    }
+     });
+})
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let selectedindex = users.findIndex(user => user.id == id);
+    if (selectedindex !== -1) {
+        users.splice(selectedindex, 1);
+        res.json({ 
+            message: 'User deleted successfully',
+            data:{deletedIndex: selectedindex}
+        });
+    } else {
+        res.status(404).json({ 
+            message: 'User not found' });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`server runing is portnpx:${port}`);
 });

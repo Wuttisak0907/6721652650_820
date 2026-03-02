7fdb32b8ddf89c7f358f7ee8fc8172d638b72cdd
Wuttisak0907 <wuttisak.ka@ku.th>
2026-03-02 18:00:14 +0700

    Update index.js
    Refactor submitData function to use async/await and handle form submission with axios.

--- Fontend/index.js
+++ Fontend/index.js
@@ -1,27 +1,47 @@
-function submitData() {
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]');
-    let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
-    let descriptionDOM = document.querySelector('textarea[name=description]');
+const submitData = async () => { 
+    
+    let messageDom = document.getElementById('message');
 
-    let interest = '';
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value;
-        if (i != interestDOMs.length - 1) {
-            interest += ', ';
-        }
+    try {
+        let fname = document.querySelector('input[name="firstname"]').value;
+        let lname = document.querySelector('input[name="lastname"]').value;
+        let age = document.querySelector('input[name="age"]').value;
+        let desc = document.querySelector('textarea[name="description"]').value;
+
+        let genderElement = document.querySelector('input[name="gender"]:checked');
+        let gender = genderElement ? genderElement.value : "ไม่ได้ระบุ";
+
+        let interests = [];
+        // แก้เป็น "interests" ให้ตรงกับ HTML
+        let hobbyElements = document.querySelectorAll('input[name="interests"]:checked'); 
+        hobbyElements.forEach((item) => {
+            interests.push(item.value);
+        });
+
+        let userData = {
+            firstname: fname,
+            lastname: lname,
+            age: age,
+            gender: gender,
+            interests: interests.join(", "), 
+            description: desc
+        };
+
+        console.log("กำลังส่งข้อมูล:", userData);
+
+        const response = await axios.post('http://localhost:8000/users', userData);
+
+        console.log("Response จาก Server:", response.data);
+        
+       
+        messageDom.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDom.className = 'message success'; 
+
+    } catch (error) {
+        console.error("เกิดข้อผิดพลาด:", error);
+        
+       
+        messageDom.innerText = 'เกิดข้อผิดพลาดในการบันทึกข้อมูล';
+        messageDom.className = 'message danger'; 
     }
-
-    let userData = {
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
-    }
-
-    console.log('submitData', userData);
 }
